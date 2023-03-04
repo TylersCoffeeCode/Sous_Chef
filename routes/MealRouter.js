892220d30a5f409e2b171d466f7af7eac781dc4b
adam <adammontemurro@gmail.com>
2023-03-04 15:28:34 -0500

    added controller and route to get meals

--- routes/MealRouter.js
+++ routes/MealRouter.js
@@ -2,5 +2,6 @@ const router = require('express').Router()
 const controller = require('../controllers/MealController')
 
 router.post('/create', controller.CreateMeal)
+router.get('/', controller.GetMeal)
 
 module.exports = router
