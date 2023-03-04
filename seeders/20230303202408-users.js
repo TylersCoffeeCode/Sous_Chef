43c809a42b233f69ceaccac65a78a8f4df7a94ba
adam <adammontemurro@gmail.com>
2023-03-04 17:19:19 -0500

    added basic seed files

--- seeders/20230303202408-users.js
+++ seeders/20230303202408-users.js
@@ -1,18 +1,23 @@
 'use strict';
+const { hashPassword } = require('../middleware')
+const falso = require('@ngneat/falso')
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
+  async up(queryInterface, Sequelize) {
 
-    
+    let users = [...Array(10)].map((_) => ({
+      username: falso.randUserName(),
+      email: falso.randEmail(),
+      password: falso.randPassword(),
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }))
+
+    await queryInterface.bulkInsert('users', users)
   },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('users')
   }
 };
