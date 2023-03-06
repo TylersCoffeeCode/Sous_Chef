c5a62fb0d861d739d8d85a9923557baf8fbb6e51
anthony5321 <tyharpo12@gmail.com>
2023-03-06 15:05:27 -0500

    Added the mealId and GetMeal controllers

--- routes/MealRouter.js
+++ routes/MealRouter.js
@@ -1,8 +1,8 @@
 const router = require('express').Router()
 const controller = require('../controllers/MealController')
-
 router.post('/create', controller.CreateMeal)
 router.get('/', controller.GetMeal)
-router.get('/:name', controller.getMealByName)
+router.get('/search/:name', controller.getMealByName)
+router.get('/:id', controller.getMealById)
 
 module.exports = router
