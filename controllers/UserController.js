f2139ad28b7c589c42178772c79741c583b29e9e
anthony5321 <tyharpo12@gmail.com>
2023-03-07 10:26:52 -0500

    Added the login

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -15,6 +15,66 @@ const Register = async (req, res) => {
   } 
 }
 
+const Login = async (req, res) => {
+  try {
+    const { email, password } = req.body
+    const user = await User.findOne({
+      where: { email: email },
+      raw: true
+    })
+    let matched = await middleware.comparePassword(
+      user.password,
+      password
+    )
+    console.log(matched);
+    if (matched) {
+      let payload = {
+        id: user.id,
+        email: user.email
+      }
+      let token = middleware.createToken(payload)
+      return res.send({ user: payload, token })
+    }
+    res.status(401).send({ status: 'Error', msg: 'Incorrect Password' })
+  } catch (error) {
+    console.log(error)
+    res.status(401).send({ status: 'Error', msg: 'An error has occurred on Login!' })
+  }
+}
+
+const UpdatePassword = async (req, res) => {
+  try {
+    const { oldPassword, newPassword } = req.body
+    const user = await User.findByPk(req.params.user_id)
+    let matched = await middleware.comparePassword(
+      user.password,
+      oldPassword
+    )
+    if (matched) {
+      let password = await middleware.hashPassword(newPassword)
+      await user.update({ password })
+      let payload = {
+        id: user.id,
+        email: user.email
+      }
+      return res.send({ status: 'Password Updated!', user: payload })
+    }
+    res.status(401).send({ status: 'Error', msg: 'Old Password did not match!' })
+  } catch (error) {
+    console.log(error)
+    res.status(401).send({ status: 'Error', msg: 'An error has occurred updating password!' })
+  }
+}
+
+const CheckSession = async (req, res) => {
+  const { payload } = res.locals
+  res.send(payload)
+}
+
+
 module.exports = {
-  Register
+  Register,
+  Login,
+  UpdatePassword,
+  CheckSession
 }
