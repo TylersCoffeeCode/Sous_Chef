4f43dcfd8c6d7a4c05912dd78a4d95c8ad583d97
adam <adammontemurro@gmail.com>
2023-03-08 10:20:04 -0500

    protected create meal route

--- routes/MealRouter.js
+++ routes/MealRouter.js
@@ -1,6 +1,11 @@
 const router = require('express').Router()
 const controller = require('../controllers/MealController')
-router.post('/create', controller.CreateMeal)
+const middleware = require('../middleware')
+
+router.post('/create',
+  middleware.stripToken,
+  middleware.verifyToken,
+  controller.CreateMeal)
 router.get('/', controller.GetMeal)
 router.get('/search/:name', controller.getMealByName)
 router.get('/:id', controller.getMealById)
