1a2972625c8bda3b3adca38972eba22ec4b5faaa
adam <adammontemurro@gmail.com>
2023-03-09 11:27:52 -0500

    added route protection for delete and update meals

--- routes/MealRouter.js
+++ routes/MealRouter.js
@@ -3,14 +3,22 @@ const controller = require('../controllers/MealController')
 const middleware = require('../middleware')
 
 router.post('/create',
-  // middleware.stripToken,
-  // middleware.verifyToken,
+  middleware.stripToken,
+  middleware.verifyToken,
   controller.CreateMeal)
 router.get('/', controller.GetMeal)
 router.get('/search/:name', controller.getMealByName)
 router.get('/:cuisine', controller.getMealByCuisine)
 router.get('/:id', controller.getMealById)
-router.delete('/:id',controller.DeleteMeal)
-router.put('/:id', controller.EditMeal)
+
+router.delete('/:id',
+middleware.stripToken,
+middleware.verifyToken,
+controller.DeleteMeal)
+
+router.put('/:id',
+middleware.stripToken,
+middleware.verifyToken,
+controller.EditMeal)
 
 module.exports = router
