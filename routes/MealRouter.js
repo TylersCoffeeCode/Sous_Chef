77965065b20f7e6dee56b2c39dd4a4fe0f4c2fed
adam <adammontemurro@gmail.com>
2023-03-09 09:19:32 -0500

    added in controllers to delete and modify specific meals

--- routes/MealRouter.js
+++ routes/MealRouter.js
@@ -3,12 +3,14 @@ const controller = require('../controllers/MealController')
 const middleware = require('../middleware')
 
 router.post('/create',
-  middleware.stripToken,
-  middleware.verifyToken,
+  // middleware.stripToken,
+  // middleware.verifyToken,
   controller.CreateMeal)
 router.get('/', controller.GetMeal)
 router.get('/search/:name', controller.getMealByName)
 router.get('/:cuisine', controller.getMealByCuisine)
 router.get('/:id', controller.getMealById)
+router.delete('/:id',controller.DeleteMeal)
+router.put('/:id', controller.EditMeal)
 
 module.exports = router
