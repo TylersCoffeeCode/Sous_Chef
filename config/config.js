54597525d7ab7e796b74e93da91d0bd1e9c0e332
anthony5321 <tyharpo12@gmail.com>
2023-03-09 20:22:35 -0500

    hot fixes for the edit meal and create meal

--- config/config.js
+++ config/config.js
@@ -2,7 +2,9 @@ require('dotenv').config()
 module.exports = {
   development: {
     database: 'Sous_Chef',
-    dialect: 'postgres'
+    dialect: 'postgres',
+    // username: "postgres",
+    // password: "1234"
   },
   test: {
     database: 'Sous_Chef',
